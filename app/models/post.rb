83a6bd0fbbf9189c7a871d3441eaad92e97f6403
github username <logemerson@gmail.com>
2017-09-06 07:17:27 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  # validates :title, allow_blank: false
+  validates :title, length: { minimum: 1 }
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
 end
